9f84fa7698a9fab6a9da434b42498b1121631421
kgoon <ehdwls4938@naver.com>
2022-10-23 03:33:21 +0900

    itemtag

--- Assets/Siwon/Script/Item.cs
+++ Assets/Siwon/Script/Item.cs
@@ -24,7 +24,7 @@ public class Item : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Garbage") || isThrowing)
+        if (collision.CompareTag("Garbage") && isThrowing)
         {
             switch (itemType)
             {
