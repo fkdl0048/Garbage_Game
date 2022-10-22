10d489b36d524a8c997f95bdb3bee0c4e08a7f2f
Jeongan Lee <84510455+fkdl0048@users.noreply.github.com>
2022-10-22 21:39:35 +0900

    Test Scene

--- Assets/JA/Spawner.cs
+++ Assets/JA/Spawner.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
 
 public class Spawner : MonoBehaviour
@@ -9,6 +10,13 @@ public class Spawner : MonoBehaviour
     [SerializeField] private bool isSpawn = true;
     [SerializeField] private float spawnDelay = 1f;
     [SerializeField] private float spawnRandominterval = 0.2f;
+    
+    [Header("Item")]
+    [SerializeField] private GameObject[] itemList;
+    [SerializeField] private float itemDelay = 10f;
+    [SerializeField] private float itemRandominterval = 1f;
+    private float curTime;
+    private bool isItem = false;
 
     private Sprite[] sprites;
     private void Awake()
@@ -21,21 +29,42 @@ public class Spawner : MonoBehaviour
         StartCoroutine(Spawn());
     }
 
+    private void Update()
+    {
+        curTime += Time.deltaTime;
+
+        if (curTime >= itemDelay && !isItem)
+        {
+            curTime = 0f;
+            isItem = true;
+        }
+    }
+
     private IEnumerator Spawn()
     {
         while (isSpawn)
         {
             Vector3 pos = new Vector3(transform.position.x - (transform.localScale.x/2) + Random.Range(0, transform.localScale.x), transform.position.y, 0);
-            GameObject go = new GameObject("Garbage");
-            go.transform.position = pos;
-            go.transform.rotation = Quaternion.Euler(1,1,Random.Range(0,360));
-            go.tag = "Garbage";
-            go.AddComponent<Rigidbody2D>();
-            go.AddComponent<SpriteRenderer>();
-            go.GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0,(sprites.Length))];
-            go.AddComponent<PolygonCollider2D>();
-            go.AddComponent<Dust>();
-            yield return new WaitForSeconds(spawnDelay + (Random.Range(-spawnRandominterval, spawnRandominterval)));
+            if (isItem)
+            {
+                isItem = false;
+                Instantiate(itemList[Random.Range(0, itemList.Length)], pos,
+                    Quaternion.Euler(1, 1, Random.Range(0, 360)));
+                yield return new WaitForSeconds(itemDelay + (Random.Range(-itemRandominterval, itemRandominterval)));
+            }
+            else
+            {
+                GameObject go = new GameObject("Garbage");
+                go.transform.position = pos;
+                go.transform.rotation = Quaternion.Euler(1,1,Random.Range(0,360));
+                go.tag = "Garbage";
+                go.AddComponent<Rigidbody2D>();
+                go.AddComponent<SpriteRenderer>();
+                go.GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0,(sprites.Length))];
+                go.AddComponent<PolygonCollider2D>();
+                go.AddComponent<Dust>();
+                yield return new WaitForSeconds(spawnDelay + (Random.Range(-spawnRandominterval, spawnRandominterval)));
+            }
         }
     }
 }
